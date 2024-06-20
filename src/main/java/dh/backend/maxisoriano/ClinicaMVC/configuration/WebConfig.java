d8548baa1a66786f6b930102b54819cfb3505d5a
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2024-06-20 20:16:52 -0300

    Cambio en WebConfig

--- src/main/java/dh/backend/maxisoriano/ClinicaMVC/configuration/WebConfig.java
+++ src/main/java/dh/backend/maxisoriano/ClinicaMVC/configuration/WebConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500/")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT","DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
